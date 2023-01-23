b80301724c7e908aa7b01b5797ef552c675e5118
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-01-23 09:02:40 +0100

    Refinements to server
    Supports MEI basic export now

--- export_server/server.py
+++ export_server/server.py
@@ -7,9 +7,25 @@ app = Sanic("export-server")
 
 @app.post("/mei")
 async def convert(req):
-    vrv_tk = verovio.toolkit()
-    uploaded_file: request.File = req.files.get("content")
-    vrv_tk.loadData(uploaded_file.body.decode("utf-8"))
+    if "content" not in req.files:
+        return response.text("No file attachment was found.", status=400)
 
-    mei = vrv_tk.getMEI({})
+    uploaded_file: request.File = req.files.get("content")
+    file_data: str = uploaded_file.body.decode("utf-8")
+    if not file_data:
+        return response.text("No data was found in the file attachment.", status=400)
+
+    vrv_tk = verovio.toolkit()
+    loaded: bool = vrv_tk.loadData(file_data)
+    if not loaded:
+        return response.text("Verovio could not load the file.", status=400)
+
+    basic_q: str = req.args.get("basic", "false")
+    basic: bool
+    if basic_q == "true":
+        basic = True
+    else:
+        basic = False
+
+    mei: str = vrv_tk.getMEI({"basic": basic})
     return response.text(mei, headers={"Content-Type": "application/xml+mei"})
